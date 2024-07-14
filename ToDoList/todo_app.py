01fadba5340bb1bfb451a992a80cabecc28473a0
Chris Anderson <canderson@ccgs.wa.edu.au>
2024-07-14 22:43:04 +0800

    Added code to insert tasks and show tasks on page

--- ToDoList/todo_app.py
+++ ToDoList/todo_app.py
@@ -1,8 +1,5 @@
 from bottle import route, run, template
-
-##################################################
-#
-# Constants
+import sqlite3
 
 ##################################################
 #
@@ -12,6 +9,70 @@ from bottle import route, run, template
 @route('/')
 def index():
     return template('index')
+
+# Create new To Do list (remove existing list and create new empty table)
+@route('/create_todo_list')
+def create_todo_list():
+    connection = sqlite3.connect('todo_list.sqlite')
+    cursor = connection.cursor()
+
+    query = 'DROP TABLE IF EXISTS Task'
+    cursor.execute(query)
+
+    query = '''
+            CREATE TABLE Task
+                (task_id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL,
+                description TEXT,
+                completed BOOLEAN DEFAULT FALSE)
+            '''
+    cursor.execute(query)
+
+    connection.commit()
+    connection.close()
+
+    return template('index')
+
+# Insert some sample data to set up the to do list
+@route('/insert_sample_data')
+def insert_sample_data():
+    connection = sqlite3.connect('todo_list.sqlite')
+    cursor = connection.cursor()
+
+    task1 = { 'title' : 'Wash car',
+                'description': 'Take care to car wash and clean the outside',
+                'completed' : False }
+    task2 = { 'title' : 'Maths',
+                'description' : 'Complete exercise 6 on page 231',
+                'completed' : True }
+    task3 = { 'title' : 'Shopping',
+                'description' : 'Buy ingredients to cook spaghetti bolognese for dinner',
+                 'completed' : False }
+    
+    query = 'INSERT INTO Task (title, description, completed) VALUES (:title, :description, :completed)'
+    cursor.execute(query, task1)
+    cursor.execute(query, task2)
+    cursor.execute(query, task3)
+
+    connection.commit()
+    connection.close()
+
+    return template('index')
+
+# Retrieve all the items in the to do list and display them on the page
+@route('/show_to_do_list')
+def show_to_do_list():
+    connection = sqlite3.connect('todo_list.sqlite')
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
+    query = 'SELECT * FROM Task'
+    cursor.execute(query)
+    tasks = cursor.fetchall()
+    
+    connection.close()
+
+    return template('index', records=tasks)
 
 ##################################################
 #
--- ToDoList/views/index.tpl
+++ ToDoList/views/index.tpl
@@ -5,6 +5,38 @@
     </head>
     <body>
         <h1>My To Do List</h1>
-        <footer>A To Do list using the Bottle framework</footer>
+        <section>
+            <p><a href="/create_todo_list">Create new To Do List</a></p>
+        </section>
+        <section>
+            <p><a href="/insert_sample_data">Insert Sample Tasks into the To Do List</a></p>
+        </section>
+        <section>
+            <p><a href="/show_to_do_list">Show the current To Do Items</a></p>
+            <h3>My Tasks</h3>
+            % if defined('records'):
+                % if len(records) < 1:
+                <p><strong>You have no items in your To Do list</strong></p>
+                % else:
+                <table>
+                    <tr>
+                        % for field in records[0].keys():
+                        <th> {{ field }} </th>
+                        % end
+                    </tr>
+                    % for record in records:
+                    <tr>
+                        % for field in record:
+                        <td>{{ field }}</td>
+                        % end
+                    </tr>
+                    % end
+                </table>
+                % end
+            % end
+        </section>
+        <footer>
+            <p>A To Do list using the Bottle framework</p>
+        </footer>
     </body>
 </html>
